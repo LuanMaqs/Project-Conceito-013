47c2177ba2a56c301446d6acb5e4a58961652550
Luan <lm7007fernandes@gmail.com>
2025-02-27 20:51:33 -0300

    Criacao de estrutura back-end

---
+++ Back-End/backend/src/main/java/com/backend/conceito013/backend/BackEndConceitoApplication.java
@@ -0,0 +1,13 @@
+package com.backend.conceito013.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackEndConceitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackEndConceitoApplication.class, args);
+	}
+
+}
---
+++ Back-End/backend/src/test/java/com/backend/conceito013/backend/BackEndConceitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backend.conceito013.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackEndConceitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
